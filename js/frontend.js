b7bf253c8a6594eb53e64c0a8126909139c75747
Phillipe <ph_santana@outlook.com>
2018-09-04 00:28:55 -0300

    Organizando FrontEnd

--- js/frontend.js
+++ js/frontend.js
@@ -1,27 +1,3 @@
-/***
-	Este arquivo serve para controlar os eventos da página, fazer a criação de elementos e chamar os métodos que executam as análises (léxica, sintática, semântica).
-
-
-BY: Gustavo Gimenez e Phillipe Sant'Ana
-***/
-
-
-/*************VARIÁVEIS GLOBAIS*******************/
-
-var bg 		= document.createElement("div");
-var body 	= document.querySelector("body");
-
-/************FUNÇÕES PRINCIPAIS****************/
-
-window.onload = bootstrap();
-
-function bootstrap(){
-//Dá o ponta pé inicial chamando as funções mais importantes para iniciar a aplicação
-choosePath();
-// startIcons();
-// rollTitle();
-}
-
 function choosePath(){
 	var main 		 = createDIV(["cover"]);
 	main.style.backgroundColor = "rgba(0,0,0,.85)";
