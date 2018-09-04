d069a2468782058c2ec30ec8700e6fa5ab344533
Phillipe <ph_santana@outlook.com>
2018-09-04 00:27:27 -0300

    Modularização do Controller

---
+++ js/controller.js
@@ -0,0 +1,23 @@
+/***
+	Este arquivo serve para controlar os eventos da página, fazer a criação de elementos e chamar os métodos que executam as análises (léxica, sintática, semântica).
+
+
+BY: Gustavo Gimenez e Phillipe Sant'Ana
+***/
+
+
+/*************VARIÁVEIS GLOBAIS*******************/
+
+var bg 		= document.createElement("div");
+var body 	= document.querySelector("body");
+
+/************FUNÇÕES PRINCIPAIS****************/
+
+window.onload = bootstrap();
+
+function bootstrap(){
+//Dá o ponta pé inicial chamando as funções mais importantes para iniciar a aplicação
+choosePath();
+// startIcons();
+// rollTitle();
+}
